466f291f3c55ca7079881e60fb5f50d0aabc1735
Magor Balassy <mazsihej@gmail.com>
2020-07-25 22:38:29 +0200

    Updated all components.

--- search_backend/main.py
+++ search_backend/main.py
@@ -3,17 +3,22 @@ from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-from HTMLParser import HTMLParser
 
 # Appengine imports
+import requests_toolbelt.adapters.appengine
 from flask import Flask, jsonify, render_template, request, Response
 from google.appengine.ext import ndb
 
 # My imports
-import json
+import json, logging, requests
 from models import Playlist, Track, User
 
 app = Flask(__name__)
+requests_toolbelt.adapters.appengine.monkeypatch()
+
+logging.basicConfig(filename='search.log',level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
 
 DEVELOPER_KEY = "Please use your own API key"
 YOUTUBE_API_SERVICE_NAME = "youtube"
@@ -26,7 +31,8 @@ def youtube_search(searchtext):
   search_response = youtube.search().list(
       q=searchtext,
       part="id,snippet",
-      maxResults=3
+      maxResults=10,
+      type="video"
   ).execute()
 
   videos = []
@@ -35,16 +41,36 @@ def youtube_search(searchtext):
 
   # Add each result to the appropriate list, and then return the lists of
   # matching videos.
+  logging.info('searchresult %s' % [ _["id"]["kind"] for _ in search_response.get("items",[])] )
   for search_result in search_response.get("items", []):
     if search_result["id"]["kind"] == "youtube#video":
       videos.append("%s (%s)" % (search_result["snippet"]["title"],
                                  search_result["id"]["videoId"]))
+  logging.info('Search result for %s: %s, %d elements.' %(searchtext, videos, len(videos)))
   return videos
 
 def custom_response(msg):
   response = jsonify({'results':msg})
   response.headers.add('Access-Control-Allow-Origin', '*')
   return response
+
+def create_playlist():
+  pass
+
+'''@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response'''
 
 @app.route('/rest/')
 @app.route('/rest/index')
@@ -55,34 +81,33 @@ def index():
 def search(search_string):
     return custom_response(youtube_search(search_string))
 
+
+@app.route('/rest/playlists/<string:name>', methods=['GET', 'POST'])
 @app.route('/rest/playlists', methods=['GET', 'POST'])
-def playlists():
+def playlists(name=None):
   res = []
   playlists = Playlist.query().fetch()
   if request.method == 'GET':
     if len(playlists) == 0 :
-      unc = Playlist(name='Uncategorized', visibility='public')
       unc_key = unc.put()
+      unc = Playlist(name='Uncategorized', visibility='public', drive_id=unc_key)
       playlists.append(unc)
     for playlist in playlists:
-      res.append({
-        'id': playlist.key.id(),
-        'name': playlist.name,
-        'location': playlist.visibility
-      })
+      res.append(playlist.serialize())
+    print(res)
     return custom_response(res)
   elif request.method == 'POST':
-    if 'name' in request.form:
-      if request.form['name'] in p_names:
-        return custom_response('exists')
-      else:
-        p = Playlist(name=request.form['name'])
-        p.put()
-        return custom_response(str(request.form))
+    p_names = [ p.name for p in playlists]
+    if name in p_names:
+      return custom_response('exists')
     else:
-      return custom_response('Name must be specified.')
+      r = requests.get('http://gc.maz.si/createpl/%s' % name)
+      drive_id = r.json()['results']
+      p = Playlist(name=name, drive_id=drive_id, visibility='public')
+      p.put()
+      return custom_response(p.serialize())
 
-@app.route('/rest/tracks')
+@app.route('/rest/tracks', methods=['GET', 'POST'])
 def tracks():
   tracks = Track.query().fetch()
   return str(tracks)
--- search_backend/models.py
+++ search_backend/models.py
@@ -5,9 +5,20 @@ class Track(ndb.Model):
   playlist = ndb.StringProperty()
   tags = ndb.StringProperty(repeated=True)
   youtube_id = ndb.StringProperty()
-  uploaded_by = ndb.StringProperty()
+  drive_id = ndb.StringProperty()  
+  #uploaded_by = ndb.StringProperty()
 
 class Playlist(ndb.Model):
+  
+  def serialize(self):
+    return {
+      'drive_id': self.drive_id,
+      'name': self.name,
+      'location': self.visibility
+    }
+
+  drive_id = ndb.StringProperty()
+  youtube_id = ndb.StringProperty()
   name = ndb.StringProperty()
   visibility = ndb.StringProperty()
   
