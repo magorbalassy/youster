a5d6975d1c018426eee426e1656b34402df69e0e
Magor Balassy <magor.balassy@gmail.com>
2020-09-23 16:24:26 +0200

    Moved to latest Angular. Broke up app to components. Added toolbar, home, search, menu components. Added Google sign-in. Added auth guard to protect pages requiring sign-in. Renamed and separated backend service. Modified search page.

--- youster/src/environments/environment.ts
+++ youster/src/environments/environment.ts
@@ -3,9 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiKey: "AIzaSyBCw7LKHIkVn5Sr1eEqoojTOlM_RdMjCj8"
-};
+  production: false};
 
 /*
  * For easier debugging in development mode, you can import the following file
